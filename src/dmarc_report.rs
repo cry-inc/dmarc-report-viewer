4b838c033dc8e34f837809b977d2823eddfc59f7
cry-inc <cry-inc@users.noreply.github.com>
2024-03-10 18:44:59 +0100

    Added test for DMARC parsing

--- src/dmarc_report.rs
+++ src/dmarc_report.rs
@@ -4,13 +4,13 @@
 use serde::Deserialize;
 use std::net::IpAddr;
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct DateRangeType {
     pub begin: u32,
     pub end: u32,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct ReportMetadataType {
     pub org_name: String,
     pub email: String,
@@ -21,7 +21,7 @@ pub struct ReportMetadataType {
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub enum AlignmentType {
     /// Relaxed
     r,
@@ -30,7 +30,7 @@ pub enum AlignmentType {
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub enum DispositionType {
     /// There is no preference on how a failed DMARC should be handled.
     none,
@@ -41,7 +41,7 @@ pub enum DispositionType {
     reject,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct PolicyPublishedType {
     pub domain: String,
     pub adkim: Option<AlignmentType>,
@@ -53,14 +53,14 @@ pub struct PolicyPublishedType {
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub enum DMARCResultType {
     pass,
     fail,
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub enum PolicyOverrideType {
     forwarded,
     sampled_out,
@@ -70,13 +70,13 @@ pub enum PolicyOverrideType {
     other,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct PolicyOverrideReason {
     pub r#type: PolicyOverrideType,
     pub comment: Option<String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct PolicyEvaluatedType {
     pub disposition: DispositionType,
     pub dkim: Option<DMARCResultType>,
@@ -84,14 +84,14 @@ pub struct PolicyEvaluatedType {
     pub reason: Option<Vec<PolicyOverrideReason>>,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct RowType {
     pub source_ip: IpAddr,
     pub count: u32,
     pub policy_evaluated: PolicyEvaluatedType,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct IdentifierType {
     pub envelope_to: Option<String>,
     pub envelope_from: Option<String>,
@@ -99,7 +99,7 @@ pub struct IdentifierType {
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub enum DKIMResultType {
     none,
     pass,
@@ -110,7 +110,7 @@ pub enum DKIMResultType {
     permerror,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct DKIMAuthResultType {
     pub domain: String,
     pub selector: Option<String>,
@@ -119,14 +119,14 @@ pub struct DKIMAuthResultType {
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub enum SPFDomainScope {
     helo,
     mfrom,
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub enum SPFResultType {
     none,
     neutral,
@@ -137,20 +137,20 @@ pub enum SPFResultType {
     permerror,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct SPFAuthResultType {
     pub domain: String,
     pub scope: Option<SPFDomainScope>,
     pub result: SPFResultType,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct AuthResultType {
     pub dkim: Option<Vec<DKIMAuthResultType>>,
     pub spf: Vec<SPFAuthResultType>,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct RecordType {
     pub row: RowType,
     pub identifiers: IdentifierType,
@@ -158,10 +158,70 @@ pub struct RecordType {
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct feedback {
     pub version: Option<String>,
     pub report_metadata: ReportMetadataType,
     pub policy_published: PolicyPublishedType,
     pub record: Vec<RecordType>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::File;
+
+    #[test]
+    fn test_aol_report() {
+        let reader = File::open("testdata/dmarc-reports/aol.xml").unwrap();
+        let report: feedback = serde_xml_rs::from_reader(reader).unwrap();
+
+        // Check metadata
+        assert_eq!(report.report_metadata.org_name, "AOL");
+        assert_eq!(report.report_metadata.email, "postmaster@aol.com");
+        assert_eq!(report.report_metadata.report_id, "website.com_1504828800");
+        assert_eq!(report.report_metadata.date_range.begin, 1504742400);
+        assert_eq!(report.report_metadata.date_range.end, 1504828800);
+
+        // Check policy
+        assert_eq!(report.policy_published.domain, "website.com");
+        assert_eq!(report.policy_published.adkim, Some(AlignmentType::r));
+        assert_eq!(report.policy_published.aspf, Some(AlignmentType::r));
+        assert_eq!(report.policy_published.p, DispositionType::reject);
+        assert_eq!(report.policy_published.sp, Some(DispositionType::reject));
+        assert_eq!(report.policy_published.pct, 100);
+
+        // Check record
+        assert_eq!(report.record.len(), 1);
+        let record = report.record.first().unwrap();
+        assert_eq!(record.row.source_ip.to_string(), "125.125.125.125");
+        assert_eq!(record.row.count, 1);
+        assert_eq!(
+            record.row.policy_evaluated.disposition,
+            DispositionType::none
+        );
+        assert_eq!(
+            record.row.policy_evaluated.dkim,
+            Some(DMARCResultType::pass)
+        );
+        assert_eq!(record.row.policy_evaluated.spf, Some(DMARCResultType::pass));
+        assert_eq!(record.identifiers.header_from, "website.com");
+        assert_eq!(
+            record.auth_results.dkim,
+            Some(vec![DKIMAuthResultType {
+                domain: String::from("website.com"),
+                selector: None,
+                result: DKIMResultType::pass,
+                human_result: None
+            }])
+        );
+        assert_eq!(
+            record.auth_results.spf,
+            vec![SPFAuthResultType {
+                domain: String::from("website.com"),
+                scope: Some(SPFDomainScope::mfrom),
+                result: SPFResultType::pass,
+            }]
+        );
+    }
 }
