3ecef2776faf69f9c672b03b49ac1a4db2871043
cry-inc <cry-inc@users.noreply.github.com>
2024-11-20 21:46:40 +0100

    Allow report filtering to only show reports with problems

--- ui/components/reports.js
+++ ui/components/reports.js
@@ -3,22 +3,37 @@ import { LitElement, html, css } from "lit";
 export class Reports extends LitElement {
     static properties = {
         reports: { type: Array },
+        flaggedOnly: { type: Boolean },
     };
 
     constructor() {
         super();
         this.reports = [];
+        this.flaggedOnly = false;
+        this.updateReports();
+    }
+
+    toggleFlagged() {
+        this.flaggedOnly = !this.flaggedOnly;
         this.updateReports();
     }
 
     async updateReports() {
-        const response = await fetch("reports");
+        const url = "reports" + (this.flaggedOnly ? "?flagged=true" : "");
+        const response = await fetch(url);
         this.reports = await response.json();
         this.reports.sort((a, b) => b.date_begin - a.date_begin);
     }
 
     render() {
-        return html`<dmarc-report-table .reports="${this.reports}"></dmarc-report-table>`;
+        return html`
+            <p>
+                <button @click=${this.toggleFlagged}>
+                    ${this.flaggedOnly ? "Show all Reports" : "Show only Reports with Problems"}
+                </button>
+            </p>
+            <dmarc-report-table .reports="${this.reports}"></dmarc-report-table>
+        `;
     }
 }
 
