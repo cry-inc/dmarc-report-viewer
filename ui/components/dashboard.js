965573baecae82fcc333f6d99a59771784f56078
cry-inc <cry-inc@users.noreply.github.com>
2024-12-23 15:36:13 +0100

    Use fixed predefined colors for Dashboard results

--- ui/components/dashboard.js
+++ ui/components/dashboard.js
@@ -52,32 +52,53 @@ export class Dashboard extends LitElement {
         const response = await fetch("summary");
         const summary = await response.json();
 
+        const colorMap = {
+            "none": "rgb(108, 117, 125)",
+            "fail": "rgb(220, 53, 69)",
+            "pass": "rgb(25, 135, 84)",
+            "softfail": "rgb(255, 193, 7)",
+            "policy": "rgb(13, 110, 253)",
+            "neutral": "rgb(13, 202, 240)",
+            "temperror": "rgb(253, 126, 20)",
+            "permerror": "rgb(132, 32, 41)",
+        };
+
         this.mails = summary.mails;
         this.xmlFiles = summary.xml_files;
         this.reports = summary.reports;
         this.lastUpdate = summary.last_update;
 
-        this.createPieChart("orgs_chart", summary.orgs, function (label) {
+        this.createPieChart("orgs_chart", summary.orgs, null, function (label) {
             window.location.hash = "#/reports?org=" + encodeURIComponent(label);
         });
-        this.createPieChart("domains_chart", summary.domains, function (label) {
+        this.createPieChart("domains_chart", summary.domains, null, function (label) {
             window.location.hash = "#/reports?domain=" + encodeURIComponent(label);
         });
-        this.createPieChart("spf_policy_chart", summary.spf_policy_results);
-        this.createPieChart("dkim_policy_chart", summary.dkim_policy_results);
-        this.createPieChart("spf_auth_chart", summary.spf_auth_results);
-        this.createPieChart("dkim_auth_chart", summary.dkim_auth_results);
+        this.createPieChart("spf_policy_chart", summary.spf_policy_results, colorMap);
+        this.createPieChart("dkim_policy_chart", summary.dkim_policy_results, colorMap);
+        this.createPieChart("spf_auth_chart", summary.spf_auth_results, colorMap);
+        this.createPieChart("dkim_auth_chart", summary.dkim_auth_results, colorMap);
     }
 
-    async createPieChart(canvasId, dataMap, onLabelClick) {
+    async createPieChart(canvasId, dataMap, colorMap, onLabelClick) {
         const element = this.renderRoot.querySelector("." + canvasId);
+
         const labels = Object.keys(dataMap);
         const data = labels.map(k => dataMap[k]);
+
+        let colors = undefined;
+        if (colorMap !== undefined && colorMap !== null) {
+            colors = labels.map(l => colorMap[l]);
+        }
+
         new Chart(element, {
             type: "pie",
             data: {
                 labels,
-                datasets: [{ data }]
+                datasets: [{
+                    data: data,
+                    backgroundColor: colors
+                }],
             },
             options: {
                 onClick: function (event, element, chart) {
