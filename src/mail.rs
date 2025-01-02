d500290b68c917a7b98c478aa203c8663bab751b
cry-inc <cry-inc@users.noreply.github.com>
2025-01-02 18:50:15 +0100

    Fixed bug with wrong suffix implementation in E-Mail subject decoding (see issue #6)

--- src/mail.rs
+++ src/mail.rs
@@ -1,4 +1,5 @@
-use base64::{engine::general_purpose::STANDARD_NO_PAD, Engine};
+use base64::engine::general_purpose::STANDARD;
+use base64::Engine;
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -22,19 +23,35 @@ pub struct Mail {
     pub parsing_errors: usize,
 }
 
-/// Basic decoder for MIME Encoded Words with UTF8 and Base64
+/// Basic decoder for MIME Encoded Words (only UTF-8 and Base64 are supported)
 pub fn decode_subject(value: String) -> String {
     const PREFIX: &str = "=?utf-8?B?";
-    const SUFFIX: &str = "=?=";
+    const SUFFIX: &str = "?=";
     if value.starts_with(PREFIX) && value.ends_with(SUFFIX) {
         let b64 = value.strip_prefix(PREFIX).expect("Failed to remove prefix");
         let b64 = b64.strip_suffix(SUFFIX).expect("Failed to remove suffix");
-        if let Ok(bytes) = STANDARD_NO_PAD.decode(b64) {
+        if let Ok(bytes) = STANDARD.decode(b64) {
             String::from_utf8(bytes).unwrap_or(value)
         } else {
             value
         }
     } else {
         value
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn decode_subject_test() {
+        assert_eq!(decode_subject(String::from("")), "");
+        assert_eq!(decode_subject(String::from("basic 123")), "basic 123");
+        assert_eq!(decode_subject(String::from("=?utf-8?B?dGV4dA==?=")), "text");
+        assert_eq!(
+            decode_subject(String::from("=?utf-8?B?YWJjZGVm?=")),
+            "abcdef"
+        );
     }
 }
