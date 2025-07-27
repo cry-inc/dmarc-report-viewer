1cfeeac82eac601127a7a7b85671950925f0fdbe
cry-inc <cry-inc@users.noreply.github.com>
2025-07-27 21:20:42 +0200

    Updated sourced UI to support both DMARC and TLS reports

--- ui/components/sources.js
+++ ui/components/sources.js
@@ -34,6 +34,10 @@ export class Sources extends LitElement {
             this.sources = this.sources.filter(s => s.issues.length > 0);
             this.filtered = true;
         }
+        if (this.params.type) {
+            this.sources = this.sources.filter(s => s.types.includes(this.params.type));
+            this.filtered = true;
+        }
     }
 
     prepareIssueBadges(issues) {
@@ -43,15 +47,51 @@ export class Sources extends LitElement {
         // Convert to nice bades with tool tips
         return issues.map(issue => {
             if (issue === "SpfPolicy") {
-                return html`<span class="badge badge-negative help" title="DMARC SPF Policy Issue">SPF Policy</span> `
+                return html`<span class="badge badge-negative">SPF Policy</span> `;
             } else if (issue === "SpfAuth") {
-                return html`<span class="badge badge-negative help" title="DMARC SPF Auth Issue">SPF Auth</span> `
+                return html`<span class="badge badge-negative">SPF Auth</span> `;
             } else if (issue === "DkimPolicy") {
-                return html`<span class="badge badge-negative help" title="DMARC DKIM Policy Issue">DKIM Policy</span> `
+                return html`<span class="badge badge-negative">DKIM Policy</span> `;
             } else if (issue === "DkimAuth") {
-                return html`<span class="badge badge-negative help" title="DMARC DKIM Auth Issue">DKIM Auth</span> `
+                return html`<span class="badge badge-negative">DKIM Auth</span> `;
+            } else if (issue === "StarttlsNotSupported") {
+                return html`<span class="badge badge-negative">No STARTTLS Support</span> `;
+            } else if (issue === "CertificateHostMismatch") {
+                return html`<span class="badge badge-negative">Certificate Mismatch</span> `;
+            } else if (issue === "CertificateExpired") {
+                return html`<span class="badge badge-negative">Certificate Expired</span> `;
+            } else if (issue === "CertificateNotTrusted") {
+                return html`<span class="badge badge-negative">No Certificate Trust</span> `;
+            } else if (issue === "ValidationFailure") {
+                return html`<span class="badge badge-negative">Validation Failure</span> `;
+            } else if (issue === "TlsaInvalid") {
+                return html`<span class="badge badge-negative">TLSA Invalid</span> `;
+            } else if (issue === "DnssecInvalid") {
+                return html`<span class="badge badge-negative">DNSSEC Invalid</span> `;
+            } else if (issue === "DaneRequired") {
+                return html`<span class="badge badge-negative">DANE Required</span> `;
+            } else if (issue === "StsPolicyFetchError") {
+                return html`<span class="badge badge-negative">STS Policy Fetch Error</span> `;
+            } else if (issue === "StsPolicyInvalid") {
+                return html`<span class="badge badge-negative">STS Policy Invalid</span> `;
+            } else if (issue === "StsWebpkiInvalid") {
+                return html`<span class="badge badge-negative">STS WebPKI Invalid</span> `;
             } else {
-                html`<span class="badge badge-negative">${issue}</span> `
+                return html`<span class="badge badge-negative">${issue}</span> `;
+            }
+        })
+    }
+
+    prepareTypesBadges(types) {
+        // Sort to always have the same badge order
+        types.sort();
+
+        // Convert to nice bades with tool tips
+        return types.map(type => {
+            if (type === "Tls") {
+                return html`<span class="badge">SMTP TLS</span> `;
+            } else if (type === "Dmarc") {
+                return html`<span class="badge">DMARC</span> `;
             }
         })
     }
@@ -62,7 +102,9 @@ export class Sources extends LitElement {
             <div>
                 ${this.filtered ?
                     html`Filter active! <a class="ml button" href="#/sources">Show all Sources</a>` :
-                    html`Filters: <a class="ml button" href="#/sources?issues=true">Only Sources with Issues</a>`
+                    html`Filters: <a class="ml button" href="#/sources?issues=true">Only Sources with Issues</a>
+                    <a class="ml button" href="#/sources?type=Dmarc">Only Sources from DMARC Reports</a>
+                    <a class="ml button" href="#/sources?type=Tls">Only Sources from SMTP TLS Reports</a>`
                 }
             </div>
             <table>
@@ -70,6 +112,7 @@ export class Sources extends LitElement {
                     <th>IP Address</th>
                     <th class="help" title="Number of records from reports for this IP">Count</th>
                     <th class="sm-hidden">Domain</th>
+                    <th class="sm-hidden help" title="Report Types">Types</th>
                     <th class="xs-hidden help" title="Issues detected in reports from this IP">Issues</th>
                 </tr>
                 ${this.sources.length !== 0 ? this.sources.map((source) =>
@@ -77,10 +120,11 @@ export class Sources extends LitElement {
                         <td>${source.ip}</a></td>
                         <td>${source.count}</td>
                         <td class="sm-hidden"><a href="#/sources?domain=${encodeURIComponent(source.domain)}">${source.domain}</a></td>
+                        <td class="sm-hidden">${this.prepareTypesBadges(source.types)}</td>
                         <td class="xs-hidden">${this.prepareIssueBadges(source.issues)}</td>
                     </tr>`
                 ) : html`<tr>
-                        <td colspan="4">No sources found.</td>
+                        <td colspan="5">No sources found.</td>
                     </tr>`
             }
             </table>
